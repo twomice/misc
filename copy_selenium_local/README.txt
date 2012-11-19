283f4f43e3a498b834d7778ae30f8f63d8e94eab
Allen Shaw <allen@nswebsolutions.com>
2012-11-19 11:32:40 -0600

    Added copy_selenium_local directory.

---
+++ copy_selenium_local/README.txt
@@ -0,0 +1,3 @@
+INSTALLATION:
+1. Rename copy_selenium_config.default.sh to copy_selenium_config.sh
+2. Edit copy_selenium_config.sh to set variables as documented inline.
---
+++ copy_selenium_local/copy_selenium_config.default.sh
@@ -0,0 +1,18 @@
+###############
+# CONFIG START:
+#
+# Source directory for Selenium tests (shared).
+rsync_source=
+
+# Local directory for Selenium tests (not shared).
+rsync_target=
+
+# Correct local path for onepager (must escape slashes)
+onepager_path=
+
+# Selenium variable for upload file.
+varname=
+
+#
+# CONFIG END.
+###############

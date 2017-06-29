5696b16639de1fd2f189cef29f91589f7493a729
Allen Shaw <allen@JoineryHQ.com>
2017-06-29 16:43:08 -0500

    Added civicrm safedev.php, for disabling certain civicrm features.

---
+++ civicrm/safedev.php
@@ -0,0 +1,23 @@
+<?php
+
+civicrm_initialize();
+
+
+// Change outbound mail setting to mail();
+$mailing_backend = Civi::settings()->get('mailing_backend');
+$mailing_backend['outBound_option'] = 3;
+Civi::settings()->set('mailing_backend', $mailing_backend);
+
+
+// Disable all non-dummy processors.
+$result = civicrm_api3('PaymentProcessor', 'get', array(
+  'sequential' => 1,
+  'payment_processor_type_id' => array('!=' => "Dummy"),
+));
+foreach ($result['values'] as $value) {
+  $params = array(
+    'id' => $value['id'],
+    'is_active' => 0,
+  );
+  civicrm_api3('PaymentProcessor', 'create', $params);
+} 

e02954383defe02584bc3956ffd1305d966c1a05
root <lucky091588@gmail.com>
2020-08-06 02:25:05 +0800

    Add mysqldumpsplitter.sh in dumpall.sh

--- data_management/mysqldumpall/dumpall.sh
+++ data_management/mysqldumpall/dumpall.sh
@@ -10,12 +10,18 @@ single_dump_file=$(mktemp);
 target_dir="" # Actually, this should come from an argument on the command line.
 
 # dump all mysqldatabases to a single file in a single transaction
-mysqldump -u root -p$mysql_root_password --single-transaction --all-databases > $single_dump_file
+mysqldump -u root -p"$mysql_root_password" --single-transaction --all-databases > "$single_dump_file"
 
 # Get all databases names from dumpfile
-database_names=$(grep -P '^-- Current Database' $single_dump_file | awk '{ print $NF }' | sed 's/`//g');
+database_names=$(grep -P '^-- Current Database' "$single_dump_file" | awk '{ print $NF }' | sed 's/`//g');
 for database_name in $database_names; do
-  # do something with $database_name
+  # Extract database
+  sh mysqldumpsplitter.sh --source "$single_dump_file" --extract DB --match_str "$database_name" --compression none --output_dir ./"$database_name"
+  # Delete last 3 line in the database extracted file
+  # head -n -3 ./"$database_name"/"$database_name".sql
+
+  # Extract all table in the database folder
+  sh mysqldumpsplitter.sh --source ./"$database_name"/"$database_name".sql --extract ALLTABLES --decompression none --compression none --output_dir ./"$database_name"/tables
 done
 
 

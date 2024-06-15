efa30397883314c9be88d6cdbfba8d56c2c4e25b
Allen Shaw <allen@JoineryHQ.com>
2024-06-14 22:03:18 -0500

    wp_snapshot: support specifying host and port in config.

--- backup/wp_snapshot/wp_snapshot.sh
+++ backup/wp_snapshot/wp_snapshot.sh
@@ -31,9 +31,18 @@ cd ..
 wp_root_basename=$(basename $wp_root_dir);
 $sudocmd tar --exclude="${wp_root_basename}/wp-content/updraft" -czf $DIRNAME/files.tgz "$wp_root_basename";
 
+MYSQL_OPTIONS=""
+if [[ -n "$mysql_host" ]]; then
+  MYSQL_OPTIONS=" --host=$mysql_host"
+fi
+if [[ -n "$mysql_port" ]]; then
+  MYSQL_OPTIONS="$MYSQL_OPTIONS --port=$mysql_port"
+fi
+
 echo "Archiving databases ..."
 echo "  Wordpress ..."
-mysqldump -u $mysql_user --password="$mysql_password" --no-tablespaces --routines $mysql_database_wordpress | gzip > $DIRNAME/wp.sql.gz
+mysqldump -u $mysql_user --password="$mysql_password" --no-tablespaces --routines $MYSQL_OPTIONS $mysql_database_wordpress | gzip > $DIRNAME/wp.sql.gz
+
 if [[ -n $mysql_database_civicrm ]]; then
   if [[ -z "$mysql_user_civicrm" ]]; then
     mysql_user_civicrm="$mysql_user";
@@ -41,8 +50,21 @@ if [[ -n $mysql_database_civicrm ]]; then
   if [[ -z "$mysql_password_civicrm" ]]; then
     mysql_password_civicrm="$mysql_password";
   fi
+  if [[ -z "$mysql_host_civicrm" ]]; then
+    mysql_host_civicrm="$mysql_host";
+  fi
+  if [[ -z "$mysql_port_civicrm" ]]; then
+    mysql_port_civicrm="$mysql_port";
+  fi
+  MYSQL_OPTIONS=""
+  if [[ -n "$mysql_host_civicrm" ]]; then
+    MYSQL_OPTIONS=" --host=$mysql_host_civicrm"
+  fi
+  if [[ -n "$mysql_port_civicrm" ]]; then
+    MYSQL_OPTIONS="$MYSQL_OPTIONS --port=$mysql_port_civicrm"
+  fi
   echo "  CiviCRM..."
-  mysqldump -u $mysql_user_civicrm --password="$mysql_password_civicrm" --no-tablespaces --routines $mysql_database_civicrm | gzip > $DIRNAME/civicrm.sql.gz
+  mysqldump -u $mysql_user_civicrm --password="$mysql_password_civicrm" --no-tablespaces --routines $MYSQL_OPTIONS $mysql_database_civicrm | gzip > $DIRNAME/civicrm.sql.gz
 fi
 
 echo "Done. Target dir: $DIRNAME";

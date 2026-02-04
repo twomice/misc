ffa3793ca32bc8425a14bdd948efa8da05be34ff
Allen Shaw <allen@JoineryHQ.com>
2026-02-04 13:08:54 -0600

    Ephemeral: improve URL printout in case of missing configs.

--- file_management/ephemeral/functions.sh
+++ file_management/ephemeral/functions.sh
@@ -1,6 +1,12 @@
 print_dir_url() {
-  local URL=$(echo $1 | sed "s#^${BASEDIR_BASE}#${BASE_URL}#")
-  echo "URLs:"
-  echo "Directory:      $URL"
-  echo "Timestamp file: $URL/.ephemeral.timestamp"
+  if [[ -n "$BASEDIR_BASE" && -n "$BASE_URL" ]]; then
+    local URL=$(echo $1 | sed "s#^${BASEDIR_BASE}#${BASE_URL}#")
+    echo "URLs:"
+    echo "Directory:      $URL"
+    echo "Timestamp file: $URL/.ephemeral.timestamp"
+  else
+    echo "RECOMMENDED: to support printing of generated URLs, add configs for BASEDIR_BASE and BASE_URL in"
+    echo "             $MYDIR/config.sh"
+    echo "             (See $MYDIR/config.sh.dist)"
+  fi
 }
